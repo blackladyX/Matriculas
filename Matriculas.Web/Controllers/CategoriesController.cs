5a46b57b54248b5ae7253194196091ddcfd800b7
blackladyX <danielahinestroza@hotmail.com>
2022-05-20 18:06:42 -0500

    Adicion de categories

---
+++ Matriculas.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using Matriculas.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Matriculas.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoriesController(ApplicationDbContext context) { _context = context; }
+        public async Task<IActionResult> Index() {
+            return View(await _context.Categories.ToListAsync());
+        }
+    }
+}
+
