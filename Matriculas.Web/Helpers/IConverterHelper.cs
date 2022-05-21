2a607216542262e0ec86ab28e75a646a8d58148d
blackladyX <danielahinestroza@hotmail.com>
2022-05-20 19:07:34 -0500

    agregamos categories y todo funciona

---
+++ Matriculas.Web/Helpers/IConverterHelper.cs
@@ -0,0 +1,13 @@
+﻿using Matriculas.Web.Models;
+using System;
+
+namespace Matriculas.Web.Helpers
+{
+    public interface IConverterHelper
+    {
+        Category ToCategory(CategoryViewModel model, Guid imageId, bool isNew);
+
+        CategoryViewModel ToCategoryViewModel(Category category);
+    }
+
+}
